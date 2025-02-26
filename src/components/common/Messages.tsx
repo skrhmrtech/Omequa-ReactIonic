da39a434d8d60a7497c559004622eaee1ecc945f
Ruttvik Kheni <developer.ruttvikkheni@gmail.com>
2025-02-26 11:18:01 +0530

    Utilize code.

--- src/components/common/Messages.tsx
+++ src/components/common/Messages.tsx
@@ -11,6 +11,10 @@ const getMessage = (message?: string): [string?, boolean?] => {
     }
 }
 
+const checkIsImage = (image: string = ''): boolean => {
+    return image?.startsWith('data:') && image?.includes('image') && image?.includes('base64');
+}
+
 function Messages({ messages, handleFileUpload }: { messages: Array<Message>, handleFileUpload: Function }) {
     return (
         <React.Fragment>
@@ -18,32 +22,30 @@ function Messages({ messages, handleFileUpload }: { messages: Array<Message>, ha
                 messages.map((message: Message, index: number) => {
 
                     const [newMessage, isKey] = getMessage(message.text);
+                    const receiveImage = message?.receiveImage || '';
+
+                    const isImage = checkIsImage(newMessage);
+                    const isReceiveImage = checkIsImage(receiveImage);
+
                     return (
                         <React.Fragment>
                             <div key={index} className={`flex items-center mb-3 ${!message.sender ? 'justify-end' : 'justify-start'}`}>
                                 <div className={`flex items-center ${!message.sender && 'flex-row-reverse'}`}>
                                     <div className={`flex flex-col ${!message.sender ? 'items-end' : 'items-start'}`}>
-                                        <p className={`chat-message text-[#0f5999] text-wrap text-sm py-3 px-5 rounded-3xl font-semibold text-justify ${!message.sender ? 'text-white bg-[#4dacff] rounded-br-md' : 'bg-[#e5f2ff] rounded-bl-md'}`}>
-                                            {
-                                                (newMessage?.startsWith('data:') && newMessage?.includes('image') && newMessage?.includes('base64')) ?
-                                                    (
-                                                        <img src={newMessage} alt="Image" className="rounded-3xl" />
-                                                    )
-                                                    :
-                                                    newMessage
-                                            }
+                                        <p className={`chat-message text-[#0f5999] text-wrap text-sm ${isImage ? 'p-3' : 'py-3 px-5'} rounded-3xl font-semibold text-justify ${!message.sender ? 'text-white bg-[#4dacff] rounded-br-md' : 'bg-[#e5f2ff] rounded-bl-md'}`}>
+                                            {isImage ? <img src={newMessage} alt="Image" className="rounded-3xl" /> : newMessage}
                                         </p>
                                     </div>
                                 </div>
                             </div>
                             {
                                 isKey && message.text === "photo_request" && (
-                                    message?.receiveImage
+                                    isReceiveImage
                                         ?
                                         (
                                             <div className={`my-3 flex items-center ${message.sender ? 'justify-end' : 'justify-start'}`}>
-                                                <p className={`chat-message text-[#0f5999] text-wrap text-sm py-3 px-5 rounded-3xl font-semibold text-justify ${message.sender ? 'bg-[#4dacff] rounded-br-md' : 'bg-[#e5f2ff] rounded-bl-md'}`}>
-                                                    <img src={message?.receiveImage} alt="Image" className="rounded-3xl" />
+                                                <p className={`chat-message text-[#0f5999] text-wrap text-sm p-3 rounded-3xl font-semibold text-justify ${message.sender ? 'bg-[#4dacff] rounded-br-md' : 'bg-[#e5f2ff] rounded-bl-md'}`}>
+                                                    <img src={receiveImage} alt="Image" className="rounded-3xl" />
                                                 </p>
                                             </div>
                                         )
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import { useHistory, useLocation } from 'react-router';
 import {
     IonPage,
     IonContent,
@@ -7,31 +8,33 @@ import {
     IonRow,
     IonCol,
 } from '@ionic/react';
-import { useHistory, useLocation } from 'react-router';
 import { Capacitor } from '@capacitor/core';
 import { Keyboard } from '@capacitor/keyboard';
 
 import { RiSendPlaneFill } from "react-icons/ri";
 import { PiLinkBreakBold } from "react-icons/pi";
 
-import { Message } from '../../utility/Types';
-import Header from '../../components/header';
-import Messages from '../../components/common/Messages';
 import Investors from '../../components/common/Investors';
-import Input from '../../components/input/Input';
+import Messages from '../../components/common/Messages';
 import TypingIndicator from '../../components/common/TypingIndicator';
 import UploadFiles from '../../components/common/UploadFiles';
+import Header from '../../components/header';
+import Input from '../../components/input/Input';
+import { generateRandomCode } from '../../utility';
+import { Message } from '../../utility/Types';
 
 import './Chat.css';
-import { generateRandomCode } from '../../utility';
 
 const SOCKET_URL = "wss://api.omequa.com/chat";
 
 const Chat: React.FC = () => {
     const history = useHistory();
     const chatEndRef = useRef<HTMLDivElement>(null);
-    const [keyboardOpen, setKeyboardOpen] = useState(0);
+    const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+    const ws = useRef<WebSocket | null>(null);
+    const { secretCode, fullName, gender } = useLocation<{ secretCode: string; fullName: string; gender: string }>()?.state || {};
 
+    const [keyboardOpen, setKeyboardOpen] = useState(0);
     const [chatActive, setChatActive] = useState(false);
     const [inputBoxActive, setInputBoxActive] = useState(false);
     const [userLeave, setUserLeave] = useState(false);
@@ -40,125 +43,88 @@ const Chat: React.FC = () => {
     const [messages, setMessages] = useState<Array<Message>>([]);
     const [isTyping, setIsTyping] = useState(false);
 
-    const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-    const location = useLocation<{ secretCode: string; fullName: string; gender: string }>();
-    const { secretCode, fullName, gender } = location?.state || {};
-
-    const ws = useRef<WebSocket | null>(null);
-
     useEffect(() => {
-        if (!secretCode || !fullName || !gender) {
-            history.replace('/');
-            return;
-        }
+        if (!secretCode || !fullName || !gender) return history.replace('/');
 
         // Initialize WebSocket connection
         const connectWebSocket = () => {
             ws.current = new WebSocket(`${SOCKET_URL}?secretCode=${secretCode}&fullName=${fullName}&gender=${gender}`);
-
-            ws.current.onopen = () => {
-                setUserName(null);
-                setChatActive(true);
-                setUserLeave(false);
-                setMessages([]);
-            };
-
-            ws.current.onmessage = (event) => {
-                const data = JSON.parse(event.data);
-                if (data.type === "text") {
-                    if (`${data.message}`.startsWith("{") && `${data.message}`.endsWith("}")) {
-                        const jsonObj = JSON.parse(`${data.message}`);
-
-                        if (jsonObj?.fullName) setUserName(jsonObj.fullName);
-
-                        if (Object.hasOwn(jsonObj, 'isTyping')) {
-                            setIsTyping(jsonObj.isTyping);
-
-                            if (typingTimeoutRef.current) clearTimeout(typingTimeoutRef.current);
-                            if (jsonObj.isTyping) typingTimeoutRef.current = setTimeout(() => setIsTyping(false), 3000);
-
-                            return;
-                        }
-
-                        if (jsonObj?.receiveImage && jsonObj?.id) {
-                            setMessages((prevM) => prevM.map((msg) => msg?.id === jsonObj?.id ? { ...msg, receiveImage: jsonObj.receiveImage } : msg));
-                            return
-                        }
-
-                        setMessages((prevMessages) => [...prevMessages, { ...jsonObj, sender: 1 }]);
-                        return
-                    }
-                    setMessages((prevMessages) => [...prevMessages, { text: data.message, sender: 1 }]);
-                }
-            };
-
-            ws.current.onclose = () => {
-                setInputBoxActive(false);
-                setChatActive(false);
-                setUserLeave(true);
-            };
+            ws.current.onopen = () => handleConnectLeave(true);
+            ws.current.onmessage = (event) => handleMessage(event);
+            ws.current.onclose = () => handleConnectLeave(false);
         };
 
         connectWebSocket();
-
         return () => ws.current?.close();
     }, [secretCode, fullName, gender]);
 
-    useEffect(() => {
-        chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [messages, isTyping]);
+    useEffect(() => chatEndRef.current?.scrollIntoView({ behavior: "smooth" }), [messages, isTyping]);
 
     useEffect(() => {
         if (Capacitor.isNativePlatform()) {
             Keyboard.addListener('keyboardDidShow', (info) => setKeyboardOpen(info?.keyboardHeight - 50));
             Keyboard.addListener('keyboardDidHide', () => setKeyboardOpen(0));
         }
-        return () => {
-            if (typingTimeoutRef.current) clearTimeout(typingTimeoutRef.current);
-        };
+        return () => { typingTimeoutRef.current && clearTimeout(typingTimeoutRef.current) };
     }, []);
 
-    const sendMessage = (msg = message) => {
-        if (!msg.trim()) return
+    const handleConnectLeave = (isConnect: boolean) => {
+        setChatActive(isConnect);
+        setUserLeave(!isConnect);
+        isConnect && setUserName(null);
+        setInputBoxActive(false);
+        setMessages([]);
+    }
 
-        if (ws.current?.readyState === WebSocket.OPEN) {
-            ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: msg, fullName }) }));
+    const handleMessage = (event: MessageEvent) => {
+        const data = JSON.parse(event.data);
+        if (data.type !== "text") return;
 
-            setMessages([...messages, { text: msg }]);
-            setMessage('');
+        try {
+            const jsonObj = JSON.parse(data.message);
+
+            if (jsonObj?.fullName) setUserName(jsonObj.fullName);
+            if ('isTyping' in jsonObj) return handleTyping(jsonObj.isTyping);
+            if (jsonObj?.receiveImage && jsonObj?.id) return updateImage(jsonObj.id, jsonObj.receiveImage);
+
+            setMessages((prev) => [...prev, { ...jsonObj, sender: 1 }]);
+        } catch {
+            setMessages((prev) => [...prev, { text: data.message, sender: 1 }]);
         }
+    };
+
+    const handleTyping = (typing: boolean) => {
+        setIsTyping(typing);
+        if (typingTimeoutRef.current) clearTimeout(typingTimeoutRef.current);
+        if (typing) typingTimeoutRef.current = setTimeout(() => setIsTyping(false), 3000);
+    };
+
+    const updateImage = (id: string, image: string) => {
+        setMessages((prev) => prev.map((msg) => (msg?.id === id ? { ...msg, receiveImage: image } : msg)));
+    };
+
+
+    const sendMessage = (msg = message, args: Object = {}) => {
+        if (!msg.trim() || ws.current?.readyState !== WebSocket.OPEN) return;
+        ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: msg, fullName, ...args }) }));
+        setMessages([...messages, { text: msg, ...args }]);
+        setMessage('');
     };
 
     const sendTypingEvent = (isTyping: boolean) => {
-        if (ws.current?.readyState === WebSocket.OPEN) {
-            ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: null, fullName, isTyping }) }));
-        }
+        if (ws.current?.readyState !== WebSocket.OPEN) return;
+        ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: null, fullName, isTyping }) }));
     };
 
-    const handleUploadImage = (image: string | null) => {
-        if (image && ws.current?.readyState === WebSocket.OPEN) {
-            ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: image, fullName }) }));
+    const handleUploadImage = (image: string | null) => image && sendMessage(image);
 
-            setMessages([...messages, { text: image }]);
-        }
-    }
-
-    const requestForPhoto = () => {
-        if (ws.current?.readyState === WebSocket.OPEN) {
-            const id = generateRandomCode();
-            ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: "photo_request", fullName, id }) }));
-
-            setMessages([...messages, { text: "photo_request", id }]);
-        }
-    }
+    const requestForPhoto = () => sendMessage("photo_request", { id: generateRandomCode() });
 
     const handleRequestedPhotoUpload = (image: string | null, id: string) => {
-        if (image && id && ws.current?.readyState === WebSocket.OPEN) {
-            ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: null, fullName, receiveImage: image, id }) }));
-
-            setMessages((prevM) => prevM.map((msg) => msg?.id === id ? { ...msg, receiveImage: image } : msg));
-        }
+        if (!(image && id && ws.current?.readyState === WebSocket.OPEN)) return;
+        ws.current.send(JSON.stringify({ type: 'text', message: JSON.stringify({ text: null, fullName, receiveImage: image, id }) }));
+        updateImage(id, image);
+        !userLeave && !inputBoxActive && messages.length && setInputBoxActive(true)
     }
 
     return (
@@ -166,11 +132,12 @@ const Chat: React.FC = () => {
             <IonContent className="select-none flex flex-col h-full" style={{ backgroundColor: "#ffffff" }} fullscreen>
                 <IonGrid className="w-full h-full flex justify-center" style={{ height: `calc(100vh - ${keyboardOpen}px)` }}>
                     <IonRow className="w-full max-w-md px-5 flex flex-col h-full">
+                        {/* Header Section */}
                         <div className="h-[10%] flex items-end pb-2">
                             <Header title={userName ?? "User"} isBack={true} />
                         </div>
 
-                        {/* Form Section */}
+                        {/* Message Section */}
                         <IonCol className="flex-grow overflow-y-auto select-text">
                             {
                                 userName && userLeave ? (
@@ -181,15 +148,13 @@ const Chat: React.FC = () => {
                                             </div>
                                             <p className="text-[#ff4a0a] mb-3 font-semibold">{userName ?? "User"} is Disconnected</p>
                                         </div>
-
-                                        {/* Investor Section */}
                                         <Investors />
                                     </div>
                                 ) : (
                                     <div className='mt-3 p-1'>
                                         {chatActive && <Messages messages={messages} handleFileUpload={handleRequestedPhotoUpload} />}
-                                        <div ref={chatEndRef} />
                                         {isTyping && <TypingIndicator />}
+                                        <div ref={chatEndRef} />
                                     </div>
                                 )
                             }
@@ -238,10 +203,7 @@ const Chat: React.FC = () => {
                                                 setMessage(e.target.value)
                                                 sendTypingEvent(true)
                                             }}
-                                            onKeyDown={(e) => {
-                                                if (e.keyCode === 13) sendMessage(e.currentTarget.value);
-                                                else sendTypingEvent(true);
-                                            }}
+                                            onKeyDown={(e) => (e.keyCode === 13) ? sendMessage(e.currentTarget.value) : sendTypingEvent(true)}
                                             slots={(
                                                 <IonButton size='small' fill="clear" slot="end" className='bg-[#68b2ff] my-1 rounded-lg text-white hover:text-black hover:transition duration-300' onClick={() => sendMessage(message)}>
                                                     <RiSendPlaneFill className="text-3xl m-2" />
